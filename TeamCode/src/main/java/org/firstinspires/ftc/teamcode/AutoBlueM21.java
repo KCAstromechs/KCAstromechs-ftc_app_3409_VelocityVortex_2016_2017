16c929b8df8c64dee88f1cafe03f035287d3d6de
MINA-RJ <buchholzfamily16@gmail.com>
2017-01-29 12:05:26 -0600

    Testing with alternative drive path

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlueM21.java
@@ -0,0 +1,110 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+@Autonomous(name="BeaconAutoBlueTest", group="Blue")
+public class AutoBlueM21 extends LinearOpMode {
+
+    AstroRobotBaseInterface robotBase;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robotBase = new RobotBasePolaris();
+        robotBase.initCallingOpMode(this);
+        robotBase.init(hardwareMap);
+        int pos;
+        double shiftedAvg;
+        double deltaX;
+        double correctionAngle;
+        waitForStart();
+
+        //initial drive
+        robotBase.driveStraight(5, 0);
+
+        robotBase.turn(10);
+
+        robotBase.hanShotFirst();
+        robotBase.hanShotFirst();
+
+        robotBase.turn(0);
+
+        robotBase.driveStraight(15, 0);
+
+        //turns parallel to ramp
+        robotBase.turn(38);
+
+        //second drive to align the robot to the first beacon one one axis
+        robotBase.driveStraight(24, 38);
+
+        //turns robot to face beacon
+        robotBase.turn(90);
+
+        //waits for robot to come to rest, then takes picture to determine beacon orientation
+        sleep(500);
+        pos = robotBase.takePicture();
+
+        //do some math to determine the angle the robot should drive to the beacon with
+        shiftedAvg = ((90 - robotBase.getZRotation()) * robotBase.PIXELS_PER_DEGREE) + robotBase.getLastPicBeaconAvg();
+        deltaX = (340 - shiftedAvg)/robotBase.PIXELS_PER_INCH;
+        correctionAngle = Math.toDegrees(Math.atan(deltaX/30.));
+
+        //outputs beacon info for testing purposes
+        System.out.println("px per degree: " + robotBase.PIXELS_PER_DEGREE);
+        System.out.println("lastPicBeaconAvg: " + robotBase.getLastPicBeaconAvg());
+        System.out.println("zRotation: " + robotBase.getZRotation());
+        System.out.println("shiftedAvg: " + shiftedAvg);
+        System.out.println("deltaX: " + deltaX);
+        System.out.println("correctionAngle: " + correctionAngle);
+
+        telemetry.addData("",pos);
+        telemetry.update();
+
+        System.out.println(pos);
+
+        if (pos == RobotBasePolaris.BEACON_RED_BLUE){
+            robotBase.turn(100 +(float)correctionAngle);
+            try {
+                robotBase.pushButton(100 + (int)correctionAngle, 100, 2);
+            }
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 100);
+            }
+        }
+        else if (pos == RobotBasePolaris.BEACON_BLUE_RED) {
+            try {robotBase.pushButton(90 + (int)correctionAngle, 90, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 90);
+            }
+        }
+        //TODO: test shooting
+        robotBase.turn(0);
+        robotBase.driveStraight(48, 0);
+
+        robotBase.turn(90);
+        sleep(500);
+        pos = robotBase.takePicture();
+
+        telemetry.addData("",pos);
+        telemetry.update();
+
+        //push chosen button
+        if (pos == RobotBasePolaris.BEACON_RED_BLUE){
+            robotBase.turn(108 + (float)correctionAngle);
+            try {robotBase.pushButton(108 + (int)correctionAngle, 108, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 108);
+            }
+        }
+        else if (pos == RobotBasePolaris.BEACON_BLUE_RED) {
+            try {robotBase.pushButton(90 + (int)correctionAngle, 90, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 90);
+            }
+        }
+
+        robotBase.deconstruct();
+        robotBase = null;
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoRedM21.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.sql.Time;
+
+@Autonomous(name="BeaconAutoRedTest", group="Red")
+public class AutoRedM21 extends LinearOpMode {
+
+    AstroRobotBaseInterface robotBase;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        robotBase = new RobotBasePolaris();
+        robotBase.initCallingOpMode(this);
+        robotBase.init(hardwareMap);
+        int pos;
+        double shiftedAvg;
+        double deltaX;
+        double correctionAngle;
+        waitForStart();
+
+        //initial drive
+        robotBase.driveStraight(5, 0);
+
+        robotBase.turn(15);
+
+        robotBase.hanShotFirst();
+        robotBase.hanShotFirst();
+
+        robotBase.turn(0);
+
+        robotBase.driveStraight(16, 0);
+
+        //turns parallel to ramp
+        robotBase.turn(322);
+
+        //second drive to align the robot to the first beacon one one axis
+        robotBase.driveStraight(29.6,322);
+
+        //turns robot to face beacon
+        robotBase.turn(275);
+
+        //waits for robot to come to rest, then takes picture to determine beacon orientation
+        sleep(500);
+        pos = robotBase.takePicture();
+
+        //do some math to determine the angle the robot should drive to the beacon with
+        shiftedAvg = ((270 - robotBase.getZRotation()) * robotBase.PIXELS_PER_DEGREE) + robotBase.getLastPicBeaconAvg();
+        deltaX = (340 - shiftedAvg)/robotBase.PIXELS_PER_INCH;
+        correctionAngle = Math.toDegrees(Math.atan(deltaX/30.));
+
+        //outputs beacon info for testing purposes
+        System.out.println("Beginning first beacon");
+        System.out.println(pos);
+        System.out.println("px per degree: " + robotBase.PIXELS_PER_DEGREE);
+        System.out.println("lastPicBeaconAvg: " + robotBase.getLastPicBeaconAvg());
+        System.out.println("zRotation: " + robotBase.getZRotation());
+        System.out.println("shiftedAvg: " + shiftedAvg);
+        System.out.println("deltaX: " + deltaX);
+        System.out.println("correctionAngle: " + correctionAngle);
+
+        telemetry.addData("",pos);
+        telemetry.update();
+
+        //if statement drives to the correct side of the beacon depending on the beacon orientation, then drives backwards away from beacon
+        if (pos == RobotBasePolaris.BEACON_RED_BLUE){
+            try {
+                robotBase.pushButton(270 + (int)correctionAngle, 270, 2);
+            }
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 270);
+            }
+        }
+        else if (pos == RobotBasePolaris.BEACON_BLUE_RED) {
+            robotBase.turn(280 + (float)correctionAngle);
+            try {robotBase.pushButton(280 + (int)correctionAngle, 280, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 280);
+            }
+        }
+
+
+        //begins journey to second beacon
+        robotBase.turn(0);
+        robotBase.driveStraight(45, 0);
+
+        //turns to face second beacon
+        robotBase.turn(275);
+
+        //waits for robot to come to rest, then takes picture to determine beacon orientation
+        sleep(500);
+        pos = robotBase.takePicture();
+
+        System.out.println("Beginning second beacon");
+        System.out.println(pos);
+        System.out.println(pos);
+        System.out.println("px per degree: " + robotBase.PIXELS_PER_DEGREE);
+        System.out.println("lastPicBeaconAvg: " + robotBase.getLastPicBeaconAvg());
+        System.out.println("zRotation: " + robotBase.getZRotation());
+        System.out.println("shiftedAvg: " + shiftedAvg);
+        System.out.println("deltaX: " + deltaX);
+        System.out.println("correctionAngle: " + correctionAngle);
+
+        telemetry.addData("",pos);
+        telemetry.update();
+
+        //if statement drives to the correct side of the beacon depending on the beacon orientation, then drives backwards away from beacon
+        if (pos == RobotBasePolaris.BEACON_RED_BLUE){
+            try {robotBase.pushButton(270 + (int)correctionAngle, 270, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 270);
+            }
+        }
+        else if (pos == RobotBasePolaris.BEACON_BLUE_RED) {
+            robotBase.turn(288 + (float)correctionAngle);
+            try {robotBase.pushButton(288 + (int)correctionAngle, 288, 2);}
+            catch (TimeoutException e) {
+                robotBase.driveStraight(-12, -0.5, 288);
+            }
+        }
+        robotBase.deconstruct();
+        robotBase = null;
+    }
+
+}
