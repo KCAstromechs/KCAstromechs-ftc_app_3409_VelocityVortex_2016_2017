7b36332dbe399a458fca060ffc511464de407aec
MINA-RJ <buchholzfamily16@gmail.com>
2017-03-12 17:22:41 -0500

    *commented

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarsAutoRed60.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MarsAutoRed60.java
@@ -5,47 +5,47 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name="Mars Red 60", group="Red")
 public class MarsAutoRed60 extends LinearOpMode {
-
-    RobotBaseMarsRD robotBase;
-    boolean debug = false;
+    
+    RobotBaseMarsRD robotBase;                                      //Pull in the robotBase for our use
+    boolean debug = false;                                          //Turn debug mode on or off (won't sout if off)
 
     @Override
     public void runOpMode() throws InterruptedException {
-        robotBase = new RobotBaseMarsRD();
-        robotBase.init(hardwareMap, this);
-        robotBase.initVuforia();
+        robotBase = new RobotBaseMarsRD();                          //Finish pulling in robotBase
+        robotBase.init(hardwareMap, this);                          //Call init to est. hardware map and sensors
+        robotBase.initVuforia();                                    //Open vuforia
         robotBase.setDebug(debug);
-        int pos = -1;
+        
+        int pos = -1;                                               //beacon position to 'null'
+        
+        //Variables for angle analysis before turns
         double shiftedAvg;
         double deltaX;
         double correctionAngle;
 
+        //If the shooter isn't cocked, cock it
         while (!robotBase.isCocked()) {
             robotBase.shooterHandler(false, true);
         }
 
+        //If we want debug mode, print info
         if (debug)
             System.out.println("SSS left isCocked");
-
-        robotBase.shooterHandler(false, false);
+        
+        robotBase.shooterHandler(false, false);                     //Make sure the shooter isn't moving
 
         waitForStart();
 
         if (debug)
             System.out.println("SSS reloaderPos @Start: " + robotBase.reloaderServo.getPosition());
 
-        //initial drive
+        if (opModeIsActive()) robotBase.driveStraight(25, 0);       //initial drive out towards center vortex
 
-        if (opModeIsActive()) robotBase.driveStraight(25, 0);
-
-        //turns parallel to ramp
-        if (opModeIsActive()) robotBase.turn(322);
-
-        //second drive to align the robot to the first beacon on one axis
-        if (opModeIsActive()) robotBase.driveStraight(24, 322);
-
-        //turns robot to face beacon
-        if (opModeIsActive()) robotBase.turn(275);
+        if (opModeIsActive()) robotBase.turn(322);                  //turns to be parallel to corner vortex
+        
+        if (opModeIsActive()) robotBase.driveStraight(24, 322);     //second drive to set robot in front of beacon
+        
+        if (opModeIsActive()) robotBase.turn(275);                  //turns robot to face beacon
 
         //waits for robot to come to rest, then takes picture to determine beacon orientation
         if (opModeIsActive()) sleep(500);
@@ -53,12 +53,12 @@ public class MarsAutoRed60 extends LinearOpMode {
             System.out.println("SSS heading after turn to take picture: " + robotBase.zRotation);
         if (opModeIsActive()) pos = robotBase.takePicture();
 
-        //do some math to determine the angle the robot should drive to the beacon with
+        //do some math to determine how to modify the angle the robot should drive to the beacon with, based on where the robot has determined it is in relation to the beacon
         shiftedAvg = ((270 - robotBase.getZRotation()) * robotBase.PIXELS_PER_DEGREE) + robotBase.getLastPicBeaconAvg();
         deltaX = (340 - shiftedAvg) / robotBase.PIXELS_PER_INCH;
         correctionAngle = Math.toDegrees(Math.atan(deltaX / 30.));
 
-        //outputs beacon info for testing purposes
+        //if debug, output beacon info
         if (debug) {
             System.out.println("px per degree: " + robotBase.PIXELS_PER_DEGREE);
             System.out.println("lastPicBeaconAvg: " + robotBase.getLastPicBeaconAvg());
@@ -68,10 +68,12 @@ public class MarsAutoRed60 extends LinearOpMode {
             System.out.println("correctionAngle: " + correctionAngle);
             System.out.println(pos);
         }
-
+        
+        //Tell user which position the robot believes the beacon to be in
         telemetry.addData("pos ", pos);
         telemetry.update();
 
+        //For either position, we turn to face angle we'll drive at, & try to push the button. If you time out, turn back. 
         if (pos == RobotBasePolaris.BEACON_RED_BLUE) {
             if (opModeIsActive()) robotBase.turn(270 + (float) correctionAngle);
             try {
@@ -87,15 +89,16 @@ public class MarsAutoRed60 extends LinearOpMode {
             }
         }
 
-        if (opModeIsActive()) robotBase.turn(122);
+        if (opModeIsActive()) robotBase.turn(122);                  //Turn the robot to the angle to shoot into the center vortex
 
 
+        //Shoot, then reload sequence, then shoot again
         while (opModeIsActive() && robotBase.shooterHandler(true, false)) ;
         while (opModeIsActive() && robotBase.reloadHandler(true)) ;
         while (opModeIsActive() && robotBase.shooterHandler(true, false)) ;
 
+        //Wait a hot sec, then turn everything off.
         if(opModeIsActive()) sleep(1000);
-
         robotBase.deconstruct();
         robotBase = null;
     }
