ed7f98a379adbd86316311be1f15ab04653ea8dd
MINA-RJ <buchholzfamily16@gmail.com>
2017-04-12 20:58:00 -0500

    Renamed TeleOp to current naming standards

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NovaTeleOp.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp (name="TeleOp", group="TeleOp")
-public class MarsTeleopRD extends OpMode{
+public class NovaTeleOp extends OpMode{
 
     RobotBaseMarsRD robotBase;
 
